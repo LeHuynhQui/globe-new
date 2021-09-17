5251407e0f5ee90c797b645aee967bdf7c3eaf85
LeHuynhQui <lehuynhqui68@gmail.com>
2021-09-17 16:36:15 +0700

    fix bug hien thi ten quoc gia trong khu vuc khi duoc chon

---
+++ src/redux/reducers/GlobeReducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+    selectedCountry: "",
+    region: {},
+    rotate:[0,0],
+    isDrag: false,
+    reset :false
+}
+
+export const GlobeReducer =  (state = initialState, action) => {
+    switch (action.type) {
+
+        case "GET_COUNTRY_AND_REGION": {
+            state.selectedCountry = action.selectedCountry;
+            state.region = action.region;
+            state.rotate = action.rotate
+            return { ...state }
+        }
+
+        // case "SET_DRAG_AND_CLICK": {
+        //     state.isDrag = action.isDrag;
+        //     console.log(action.isDrag)
+        //     return { ...state }
+        // }
+
+        case "RESET_COUNTRY_NAME" : {
+            return { ...state, reset :true }
+        }
+
+        default: {
+            return { ...state }
+        }
+    }
+}
